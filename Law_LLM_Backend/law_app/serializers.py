a563fb51be2274b643d2bcd39b1a383d6a7b67fd
hashir4825 <hashir4825@gmail.com>
2024-06-11 22:19:18 +0500

    Updated all components

--- Law_LLM_Backend/law_app/serializers.py
+++ Law_LLM_Backend/law_app/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
 from .models import Document
 
-class DocumentSerializer(serializers.ModelSerializer):
+class DocumentCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Document
+        exclude = ['status', 'user']
+
+
+class DocumentViewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Document
         fields = '__all__'
--- Law_LLM_Backend/law_app/urls.py
+++ Law_LLM_Backend/law_app/urls.py
@@ -6,5 +6,5 @@ from .views import DocumentCreateView, SingleDocumentView, Document_OfSpecificUs
 urlpatterns = [
     path('documents/', DocumentCreateView.as_view(), name='document-create'),
     path('documents/<int:pk>/', SingleDocumentView.as_view(), name='single-document'),
-    path('users/<int:user_id>/documents/', Document_OfSpecificUser.as_view(), name='documents-of-specific-user'),
+    path('users/documents/', Document_OfSpecificUser.as_view(), name='documents-of-specific-user'),
 ]
